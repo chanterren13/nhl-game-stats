553585e0c8c5df69356d3726e8d3b580c111fd3d
terren <terrenjchan@gmail.com>
2022-11-06 22:27:34 -0500

    Added props and styling

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -3,12 +3,13 @@ import './Game.css';
 import Team from '../Team';
 
 export default function Game() {
+    const record = {wins:"W", losses:"L", otLosses:"OTL"}
     return (
         <div>
             <h3>Game</h3>
             <div className='team-box'>
-                <Team></Team>
-                <Team></Team>
+                <Team name={"Team Name 1"} record={record}></Team>
+                <Team name={"Team Name 2"} record={record}></Team>
             </div>
         </div>
     );
--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './Team.css';
 import { useState } from 'react';
 
-export default function Team() {
+export default function Team({name, record}) {
 
     const [expanded, setExpanded] = useState(false);
 
     return (
-        <div>
-            <h3>Team Name 1</h3>
-            <h4>W-L-OTL</h4>
+        <div className='team'>
+            <h3>{name}</h3>
+            <h4>{record.wins}-{record.losses}-{record.otLosses}</h4>
             <button onClick={() => setExpanded(!expanded)}>Expand</button>
             {expanded && 
                 <div>
