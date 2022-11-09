a26a776a5867d4724136bd53d6f305654d548a19
terren <terrenjchan@gmail.com>
2022-11-08 23:23:48 -0500

    Adjust styling

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -8,9 +8,16 @@ export default function Team({name, record}) {
 
     return (
         <div className='team'>
-            <h3>{name}</h3>
-            <h4>{record.wins}-{record.losses}-{record.otLosses}</h4>
-            <button onClick={() => setExpanded(!expanded)}>Expand</button>
+            <div className='content'>
+                <div>
+                    <h3>{name}</h3>
+                    <h4>{record.wins}-{record.losses}-{record.otLosses}</h4>
+                </div>
+                <div>
+                    <button onClick={() => setExpanded(!expanded)}>Expand</button>
+                </div>
+            </div>
+            
             {expanded && 
                 <div>
                     Roster
