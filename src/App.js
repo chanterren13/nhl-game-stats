0ee064c59f48c0f6af6acf6e389cca498e313c52
terren <terrenjchan@gmail.com>
2022-11-09 21:07:59 -0500

    Added api fetch for schedule

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import Game from './components/Game';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
 function App() {
-  return (
-    <div>
-        <h1>NHL Game Stats</h1>
-        <Game></Game>
-    </div>
-  );
+
+    const [schedule, setSchedule] = useState();
+
+    useEffect(() => {
+        fetchSchedule();
+    })
+
+    const fetchSchedule = () => {
+        let config = {
+            method: 'get',
+            url: 'https://statsapi.web.nhl.com/api/v1/schedule',
+            headers: { }
+        };
+
+        axios(config)
+        .then((response) => {
+            let data = response.data;
+            //console.log(data.dates[0].games);
+            setSchedule(data.dates[0].games);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    };
+
+    return (
+        <div>
+            <h1>NHL Game Stats</h1>
+            {schedule && schedule.map((game) => 
+            <Game homeTeam={game.teams.home} awayTeam={game.teams.away}></Game>)}
+        </div>
+    );
 }
 
 export default App;
