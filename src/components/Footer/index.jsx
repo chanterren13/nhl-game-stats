2bdedb2388bfc6e61899fccd9d5b1fa672ae1183
terren <terrenjchan@gmail.com>
2023-02-20 20:03:31 -0500

    feat: updated footer styling

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ export default function Footer () {
     return (
         <div className='footer-content'>
             <p>Check the stats of every player from every night's game!</p>
-            <p>Created by: <a href='https://github.com/chanterren13'>@chanterren13</a></p>
+            <p className='signature'>Created by: <a href='https://github.com/chanterren13'>@chanterren13</a></p>
         </div>
     );
 }
