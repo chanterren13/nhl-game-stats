6ffd4a543524ba186992952576e8261e6bfce882
terren <terrenjchan@gmail.com>
2023-04-03 15:55:16 -0400

    fix: remove update script

--- scripts/updateDB.js
+++
@@ -1,9 +0,0 @@
-import { updateTeams, updatePlayers } from "./extractionUtils.js";
-
-export const updateDB = async (schedule) => {
-  const ids = parseSchedule(schedule);
-  await updateTeams(ids);
-  for (const id of ids) {
-    await updatePlayers(id);
-  }
-};
